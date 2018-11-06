cfac3bd9b2c061e2324dd4851ab4e3a769b75329
Brendan Smith <795924@seq.org>
2018-11-06 08:38:24 -0800

    b lj

--- Lab 1026 ColorBars/sketch.js
+++ Lab 1026 ColorBars/sketch.js
@@ -9,7 +9,7 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(20, 20, 20);
-  numBars = 40;
+  numBars = 500;
   loadBars(numBars);
   bubbleSort(Bars);
 }
@@ -26,11 +26,11 @@ function draw() {
 function loadBars(numBars){
   for(var i = 0; i < numBars; i++){
     var w = 800/numBars;
-    var h = 250;
+    var h = 69;
     var loc = createVector(0+(w*i), 150);
-    var red = random(0, 255);
-    var green = random(0, 255);
-    var blue = random(0, 255);
+    var red = random(0, 225);
+    var green = random(0, 175);
+    var blue = random(0, 20);
     var clr = color(red, green, blue);
     var clrAvg = (red + green+ blue)/3;
     var bar = new colorbar(loc, w, h, clr, clrAvg);
